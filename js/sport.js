843cb93d0a30abaf095f85b58e9a7a8a2d77234d
B-G-H <barbara.henn@huntheim.de>
2023-07-06 13:54:19 +0200

    Verbesserungen
    - anderer schalter
    - Anpassung Canvas 3D Objekt

--- js/sport.js
+++ js/sport.js
@@ -8,11 +8,30 @@ function wechsel2()
 {
     var bild = document.getElementById("sport");
     bild.setAttribute("src", "img/sport-3.png");
-    bild.setAttribute("alt", "farbige Version des Fotos");
+    bild.setAttribute("alt", "freigestellter Torhüter");
 }
 function original() 
 {
     var bild = document.getElementById("sport");
     bild.setAttribute("src", "img/sport-1.png");
-    bild.setAttribute("alt", "freigestellter Torhüter");
+    bild.setAttribute("alt", "Originalfoto schwarz weiß, Handballtorwart in Abwehrhaltung, Ball fliegt auf ihn zu");
+}
+
+function wechsel()
+{
+    var bild = document.getElementById("sport");
+    var qu = bild.getAttribute("src");
+
+    if (qu == "img/sport-1.png") {
+        bild.setAttribute("src", "img/sport-2.png");
+        bild.setAttribute("alt", "farbige Version des Fotos");
+    }
+    if (qu == "img/sport-2.png") {
+        bild.setAttribute("src", "img/sport-3.png");
+        bild.setAttribute("alt", "freigestellter Torhüter");
+    }
+    if (qu == "img/sport-3.png") {
+        bild.setAttribute("src", "img/sport-1.png");
+        bild.setAttribute("alt", "Originalfoto schwarz weiß, Handballtorwart in Abwehrhaltung, Ball fliegt auf ihn zu");
+    }
 }
