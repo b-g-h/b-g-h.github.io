1e2d1ac42fa1a338bd68b05bb1a211cbc0224307
B-G-H <barbara.henn@huntheim.de>
2023-07-06 18:15:11 +0200

    Code aufgeräumt

--- mbz/js/bar.js
+++ mbz/js/bar.js
@@ -1,106 +1,25 @@
 function farblos()
 {
-    var bar = document.getElementById("bar");
-    bar.setAttribute("fill", "url(#pattern-circles");
-    var key = document.getElementById("key");
-    key.setAttribute("fill", "url(#pattern-circles");
-    var bar2 = document.getElementById("bar-2");
-    bar2.setAttribute("fill", "darkgrey");
-    var key2 = document.getElementById("key-2");
-    key2.setAttribute("fill", "darkgrey");
-    var bar3 = document.getElementById("bar-3");
-    bar3.setAttribute("fill", "url(#pattern-checkers");
-    var key3 = document.getElementById("key-3");
-    key3.setAttribute("fill","url(#pattern-checkers");
-    var bar4 = document.getElementById("bar-4");
-    bar4.setAttribute("fill", "#2b2b2b");
-    var key4 = document.getElementById("key-4");
-    key4.setAttribute("fill", "#2b2b2b");
-    var bar5 = document.getElementById("bar-5");
-    bar5.setAttribute("fill", "url(#pattern-cubes");
-    var key5 = document.getElementById("key-5");
-    key5.setAttribute("fill", "url(#pattern-cubes");
-    var bar6 = document.getElementById("bar-6");
-    bar6.setAttribute("fill", "url(#pattern-lines");
-    var key6 = document.getElementById("key-6");
-    key6.setAttribute("fill", "url(#pattern-lines");
-    var bar7 = document.getElementById("bar-7");
-    bar7.setAttribute("fill", "black");
-    var key7 = document.getElementById("key-7");
-    key7.setAttribute("fill", "black");
-    var bar8 = document.getElementById("bar-8");
-    bar8.setAttribute("fill", "url(#pattern-chevron");
-    var key8 = document.getElementById("key-8");
-    key8.setAttribute("fill", "url(#pattern-chevron");
+    const pattern1 = ["#df0000", "#24c62b", "#1010b2", "#e29e14", "#7d11c9", "#f0f", "#065109", "#06b7f4"];
+    const pattern2 = ["url(#pattern-circles)", "darkgrey", "url(#pattern-checkers)", "#2b2b2b", "url(#pattern-cubes)", "url(#pattern-lines)", "black", "url(#pattern-chevron)"];
+    
+    if (document.getElementById("bar-1").getAttribute("fill") == "#df0000"){
+        for(let i=1; i<9; i++){
+            document.getElementById("bar-"+i).setAttribute("fill", pattern2[i-1]);
+            document.getElementById("key-"+i).setAttribute("fill", pattern2[i-1]);
+        }
+    } else {
+        for(let i=1; i<9; i++){
+            document.getElementById("bar-"+i).setAttribute("fill", pattern1[i-1]);
+            document.getElementById("key-"+i).setAttribute("fill", pattern1[i-1]);
+        }
+    }
 }
 function stroke()
 {
-    var bar = document.getElementById("bar");
-    bar.setAttribute("style","stroke-width:2;stroke:black")
-    var key = document.getElementById("key");
-    key.setAttribute("style","stroke-width:2;stroke:black")
-    var bar2 = document.getElementById("bar-2");
-    bar2.setAttribute("style","stroke-width:2;stroke:black")
-    var key2 = document.getElementById("key-2");
-    key2.setAttribute("style","stroke-width:2;stroke:black")
-    var bar3 = document.getElementById("bar-3");
-    bar3.setAttribute("style","stroke-width:2;stroke:black")
-    var key3 = document.getElementById("key-3");
-    key3.setAttribute("style","stroke-width:2;stroke:black")
-    var bar4 = document.getElementById("bar-4");
-    bar4.setAttribute("style","stroke-width:2;stroke:black")
-    var key4 = document.getElementById("key-4");
-    key4.setAttribute("style","stroke-width:2;stroke:black")
-    var bar5 = document.getElementById("bar-5");
-    bar5.setAttribute("style","stroke-width:2;stroke:black")
-    var key5 = document.getElementById("key-5");
-    key5.setAttribute("style","stroke-width:2;stroke:black")
-    var bar6 = document.getElementById("bar-6");
-    bar6.setAttribute("style","stroke-width:2;stroke:black")
-    var key6 = document.getElementById("key-6");
-    key6.setAttribute("style","stroke-width:2;stroke:black")
-    var bar7 = document.getElementById("bar-7");
-    bar7.setAttribute("style","stroke-width:2;stroke:black")
-    var key7 = document.getElementById("key-7");
-    key7.setAttribute("style","stroke-width:2;stroke:black")
-    var bar8 = document.getElementById("bar-8");
-    bar8.setAttribute("style","stroke-width:2;stroke:black")
-    var key8 = document.getElementById("key-8");
-    key8.setAttribute("style","stroke-width:2;stroke:black")
-}
-
-function stroke2()
-{
-    var bar = document.getElementById("bar");
-    bar.setAttribute("style","stroke-width:4;stroke:black")
-    var key = document.getElementById("key");
-    key.setAttribute("style","stroke-width:4;stroke:black")
-    var bar2 = document.getElementById("bar-2");
-    bar2.setAttribute("style","stroke-width:4;stroke:black")
-    var key2 = document.getElementById("key-2");
-    key2.setAttribute("style","stroke-width:4;stroke:black")
-    var bar3 = document.getElementById("bar-3");
-    bar3.setAttribute("style","stroke-width:4;stroke:black")
-    var key3 = document.getElementById("key-3");
-    key3.setAttribute("style","stroke-width:4;stroke:black")
-    var bar4 = document.getElementById("bar-4");
-    bar4.setAttribute("style","stroke-width:4;stroke:black")
-    var key4 = document.getElementById("key-4");
-    key4.setAttribute("style","stroke-width:4;stroke:black")
-    var bar5 = document.getElementById("bar-5");
-    bar5.setAttribute("style","stroke-width:4;stroke:black")
-    var key5 = document.getElementById("key-5");
-    key5.setAttribute("style","stroke-width:4;stroke:black")
-    var bar6 = document.getElementById("bar-6");
-    bar6.setAttribute("style","stroke-width:4;stroke:black")
-    var key6 = document.getElementById("key-6");
-    key6.setAttribute("style","stroke-width:4;stroke:black")
-    var bar7 = document.getElementById("bar-7");
-    bar7.setAttribute("style","stroke-width:4;stroke:black")
-    var key7 = document.getElementById("key-7");
-    key7.setAttribute("style","stroke-width:4;stroke:black")
-    var bar8 = document.getElementById("bar-8");
-    bar8.setAttribute("style","stroke-width:4;stroke:black")
-    var key8 = document.getElementById("key-8");
-    key8.setAttribute("style","stroke-width:4;stroke:black")
+    var strokeString = "stroke-width:2;stroke:black"
+    for(let i=1; i<9; i++){
+        document.getElementById("bar-"+i).setAttribute("style", strokeString);
+        document.getElementById("key-"+i).setAttribute("style", strokeString);
+    }
 }
