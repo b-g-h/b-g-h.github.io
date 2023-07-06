30de73fa8760642cd178423c9482bac6d7263fc5
B-G-H <barbara.henn@huntheim.de>
2023-07-06 12:38:17 +0200

    + vbs2023 (Schulbuchvortrag), dazu Bildwechsler

---
+++ js/sport.js
@@ -0,0 +1,18 @@
+function wechsel1() 
+{
+    var bild = document.getElementById("sport");
+    bild.setAttribute("src", "img/sport-2.png");
+    bild.setAttribute("alt", "farbige Version des Fotos");
+}
+function wechsel2() 
+{
+    var bild = document.getElementById("sport");
+    bild.setAttribute("src", "img/sport-3.png");
+    bild.setAttribute("alt", "farbige Version des Fotos");
+}
+function original() 
+{
+    var bild = document.getElementById("sport");
+    bild.setAttribute("src", "img/sport-1.png");
+    bild.setAttribute("alt", "freigestellter Torhüter");
+}
