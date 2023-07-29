abe8a2530d52b66f986d5c807c7e01a6dc4a9214
B-G-H <barbara.henn@huntheim.de>
2023-07-29 17:44:26 +0200

    update vbs2023.html

--- mbz/js/bar.js
+++ mbz/js/bar.js
@@ -1,7 +1,7 @@
 function farblos()
 {
     const pattern1 = ["#df0000", "#24c62b", "#1010b2", "#e29e14", "#7d11c9", "#f0f", "#065109", "#06b7f4"];
-    const pattern2 = ["url(#pattern-circles)", "darkgrey", "url(#pattern-checkers)", "#2b2b2b", "url(#pattern-cubes)", "url(#pattern-lines)", "black", "url(#pattern-chevron)"];
+    const pattern2 = ["url(#pattern-circles)", "darkgrey", "url(#pattern-checkers)", "#737373", "url(#pattern-cubes)", "url(#pattern-lines)", "black", "url(#pattern-chevron)"];
     
     if (document.getElementById("bar-1").getAttribute("fill") == "#df0000"){
         for(let i=1; i<9; i++){
